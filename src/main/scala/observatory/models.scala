06053fcc9a6737e7ed208df9618dd5f1f46fce89
aburns <anthony@clearme.com>
2017-08-26 17:02:23 -0400

    original status of code

--- src/main/scala/observatory/models.scala
+++ src/main/scala/observatory/models.scala
@@ -2,7 +2,6 @@ package observatory
 
 /**
   * Introduced in Week 1. Represents a location on the globe.
-  *
   * @param lat Degrees of latitude, -90 ≤ lat ≤ 90
   * @param lon Degrees of longitude, -180 ≤ lon ≤ 180
   */
@@ -41,29 +40,3 @@ case class CellPoint(x: Double, y: Double)
   */
 case class Color(red: Int, green: Int, blue: Int)
 
-// stations.csv  STN identifier	WBAN identifier	Latitude	Longitude
-case class StationIdentifier(stn:String, wban:String)
-case class Station(stationIdentifier: StationIdentifier, location: Location)
-case class StationRaw(stn:String, wban:String, lat: Double, lon: Double){
-  def station():Station={
-    Station(StationIdentifier(stn,wban), Location(lat, lon))
-  }
-}
-
-case class ObservationRaw(stn:String, wban:String, month:Int, day:Int, temperature: Double, year:Year){
-  def observation():ObservationStation={
-    ObservationStation(StationIdentifier(stn,wban), YMD(year,month,day), temperature)
-  }
-}
-
-// STN identifier	WBAN identifier	Month	Day	Temperature (in degrees Fahrenheit)
-case class ObservationStation(stationIdentifier: StationIdentifier, ymd: YMD, temperature: Temperature)
-
-object Observation{
-  def apply(station: Station, observationStation: ObservationStation):Observation = {
-    this(station.location, observationStation.ymd, observationStation.temperature)
-  }
-}
-case class YMD(year:Int, month:Int, day:Int)
-
-case class Observation(location: Location, ymd: YMD, temperature: Temperature)
